03eaaa2e4e8719905ced79cd40d193f2f95a3493
Virendra Mule <virendra.r.mule@gmail.com>
2024-06-03 20:11:35 +0530

    Introduction to js

---
+++ Introduction/Starter/HelloWorld.js
@@ -0,0 +1,29 @@
+// Declaration of different variables with different data types
+let firstName = 'Virendra';
+let lastName = 'Mule';
+let country = 'India';
+let age = 24
+let rollNo = 50018;
+let hight = 171.56;
+let weight = 53.1423121;
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Asabeneh',     //name of a person
+  job = 'teacher',
+  live = 'Finland';
+
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof afe);
+console.log(typeof rollNo);
+console.log(typeof hight);
+console.log(typeof weight);
